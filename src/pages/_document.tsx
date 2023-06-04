c4a3c97ec42d52ef6f0c2804769fc4fbfa884155
= <=>
2023-06-04 13:28:48 -0400

    html2pdf-with-JSON

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head >
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={undefined}/>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@500;700;1000&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;800;900&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Script from "next/script";
 import jsPDF from "jspdf";
 import { useEffect, useRef, useState } from "react";
 import ReportTemplate from "./reportTemplate";
+// import html2pdf from 'html2pdf.js';
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -14,35 +15,52 @@ export default function Home() {
 
   // const img = new Image()
 
-  const handleGeneratePdf = () => {
-    const doc = new jsPDF({
-      format: "a4",
-      unit: "px",
-    });
+  const handleGeneratePdf = async () => {
+    // const doc = new jsPDF({
+    //   format: "a4",
+    //   unit: "px",
+    // });
 
-    const img = document.createElement('img');
-    img.src = "/pudin.png";
-    const canvas = document.getElementById("chart-1") as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d");
+    // const img = document.createElement('img');
+    // img.src = "/pudin.png";
+    // const canvas = document.getElementById("chart-1") as HTMLCanvasElement;
+    // const ctx = canvas.getContext("2d");
 
-    ctx?.drawImage(img, 0, 0);
-    const imageData = canvas.toDataURL("image/png");
+    // ctx?.drawImage(img, 0, 0);
+    // const imageData = canvas.toDataURL("image/png");
 
-    const { x, y, height, width } = canvas.getBoundingClientRect();
-    doc.addImage(imageData, "PNG", x, y, x + width, y + height);
+    // const { x, y, height, width } = canvas.getBoundingClientRect();
+    // doc.addImage(imageData, "PNG", x, y, x + width, y + height);
 
-    // Adding the fonts.
-    doc.addFont("/Static/Nunito-Regular.ttf", "Nunito", "normal");
-    doc.addFont("/Static/Nunito-Bold.ttf", "Nunito", "bold");
-    doc.addFont("/Static/Nunito-ExtraBold.ttf", "Nunito", "bolditalic");
-    doc.setFont("Nunito", "normal");
+    // // Adding the fonts.
+    // doc.addFont("/Static/Nunito-Regular.ttf", "Nunito", "normal");
+    // doc.addFont("/Static/Nunito-Bold.ttf", "Nunito", "bold");
+    // doc.addFont("/Static/Nunito-ExtraBold.ttf", "Nunito", "bolditalic");
+    // doc.setFont("Nunito", "normal");
 
-    doc.html(reportTemplateRef.current, {
-      async callback(doc) {
-        setFrame(doc.output("bloburl") as any);
-        console.log(doc.getFontList());
-      },
-    });
+  //   doc.html(reportTemplateRef.current, {
+  //     async callback(doc) {
+        // setFrame(doc.output("bloburl") as any);
+  //       console.log(doc.getFontList());
+  //     },
+  //   });
+  // const $doc = document.getElementById('doc')
+  // const pdf = new html2pdf()
+  //   .set({
+  //     filename: 'Sistema de ecuaciones.pdf',
+  //     html2canvas: {
+  //       scale: 4, // A mayor escala, mejores gráficos, pero más peso
+  //       letterRendering: true,
+  //   },
+  //     jsPDF: {
+  //       unit: 'px',
+  //       format: 'a4',
+  //     }
+  //   })
+  //   .from($doc);
+  // console.log(await pdf)
+  // await pdf.save()
+  // console.log(pdf)
   };
 
   return (
@@ -54,7 +72,6 @@ export default function Home() {
         <div ref={reportTemplateRef}>
           <ReportTemplate />
         </div>
-        ({frame && <iframe className={styles.frame} src={frame}></iframe>})
       </div>
     </main>
   );
