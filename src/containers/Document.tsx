e70d6bc392de51c3bdd8ec0e158ea1e3b1942281
= <=>
2023-06-30 19:26:46 -0400

    doc fix V1.0.3

---
+++ src/containers/Document.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+import styles from "../styles/Doc.module.css";
+import getComponent from "@/utils/getComponent";
+
+export default function Document({ childrens }: { childrens: {
+  type: string;
+  options: any;
+ }[]}) {
+  console.log(childrens)
+  return (
+    <div className={styles.doc} id="doc">
+      {childrens?.map(({ type, options }) => getComponent(type, options))}
+    </div>
+  );
+}
